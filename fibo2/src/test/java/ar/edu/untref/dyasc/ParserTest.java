522f0523d5d0e8d459aa7821218b857fa672a0ba
tuxe88 <v.tourrilhes@gmail.com>
2023-09-03 21:49:27 -0300

    Agregado parser tests

---
+++ fibo2/src/test/java/ar/edu/untref/dyasc/ParserTest.java
@@ -0,0 +1,28 @@
+package ar.edu.untref.dyasc;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+
+public class ParserTest {
+
+    @Test
+    public void getFiboNumber(){
+        Parser p = new Parser();
+        String[] args = {"-o=vd","2"};
+        assertThat(p.getFiboNumber(args)).isEqualTo(2);
+    }
+
+    @Test
+    public void getOrientation(){
+        Parser p = new Parser();
+        String[] args = {"-o=vd","2"};
+        assertThat(p.getOrientation(args)).isEqualTo('v');
+    }
+
+    @Test
+    public void getOrder(){
+        Parser p = new Parser();
+        String[] args = {"-o=vd","2"};
+        assertThat(p.getOrder(args)).isEqualTo('v');
+    }
+
+}
